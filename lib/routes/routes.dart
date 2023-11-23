695dbb5a3687b57c5efcd4575a2bd1cf94a0f7ce
rr-ds <robson.rodrigues@hotmail.com>
2023-11-23 17:12:58 -0300

    4

---
+++ lib/routes/routes.dart
@@ -0,0 +1,9 @@
+import 'package:munick/view/editar_cliente_page.dart';
+import 'package:munick/view/inserir_cliente_page.dart';
+import 'package:munick/view/listar_cliente_page.dart';
+
+class Routes {
+  static const String list = ListarClientesPage.routeName;
+  static const String insert = InserirClientePage.routeName;
+  static const String edit = EditarClientePage.routeName;
+}
